6230d15d34085297cda64d495454e7497f688624
Julie Poulain <juliempoulain@gmail.com>
2024-06-14 14:04:34 -0700

    finish crud

--- server/models.py
+++ server/models.py
@@ -13,25 +13,11 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String)
 
     # # add relationships
-    # visits = db.relationship("Visit", back_populates="owner")
-    # pets = db.relationship("Pet", back_populates="owner")
-    # sitters = association_proxy("visits", "sitter")
-    # test
+    tasktemplates = db.relationship("TaskTemplate", back_populates="users")
+    routinetemplates = db.relationship("RoutineTemplate", back_populates="users")
 
-    # @hybrid_property
-    # def unique_sitters(self):
-    #     sitter_list = [visit.sitter for visit in self.visits]
-    #     unique_sitter_list = list(set(sitter_list))
-    #     return unique_sitter_list
-    # # add serialization rules
-    # serialize_rules = ('-visits.owner', '-pets.owner', 'sitters')
-
-    # # add validation
-    # @validates("phone")
-    # def validate_phone(self, _, phone):
-    #     if not isinstance(phone, int) or (len(str(phone)) != 10):
-    #         raise ValueError("Phone must be an integer with 10 characters, no spaces")
-    #     return phone
+    # add serialization rules
+    serialize_rules = ("-tasktemplates",)
 
     def __repr__(self):
         return f"<User: {self.name}>"
@@ -44,14 +30,14 @@ class TaskTemplate(db.Model, SerializerMixin):
     timer_length = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
-    # user = db.relationship('User', back_populates='tasktemplates')
-    # routinetemplates = db.relationship('Routine Template', back_populates='tasktemplates')
+    users = db.relationship("User", back_populates="tasktemplates")
+    taskroutines = db.relationship("TaskRoutine", back_populates="tasktemplates")
+    routines = association_proxy("taskroutines", "tasktemplate") 
+
+    serialize_rules = ("-taskroutines","-users")
 
     def __repr__(self):
-        return f'<Task {self.id}: {self.name}>'
-
-    # serialize_rules = ('-owner.pets', '-visits.pet', '-owner.visits')
-           
+        return f'<Task {self.id}: {self.name}>'           
     
 class RoutineTemplate(db.Model, SerializerMixin):
     __tablename__ = "routinetemplates"
@@ -63,21 +49,12 @@ class RoutineTemplate(db.Model, SerializerMixin):
     # end_time = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    # task_templates = db.relationship('Task Template', back_populates='routinetemplates')
+    # add relationships
+    users = db.relationship("User", back_populates="routinetemplates")
+    taskroutines = db.relationship("TaskRoutine", back_populates="routinetemplates")
+    tasks = association_proxy("taskroutines", "routinetemplate") 
 
-    # serialize_rules = ('-visits.sitter', '-owners.sitters', '-visits.owner', '-visits.pet')
-
-    # @validates("phone")
-    # def validate_phone(self, _, phone):
-    #     if not isinstance(phone, int) or (len(str(phone)) != 10):
-    #         raise ValueError("Phone must be an integer with 10 characters, no spaces")
-    #     return phone
-    
-    # @validates("experience")
-    # def validate_experience(self, _, experience):
-    #     if not isinstance(experience, int) or (experience > 10):
-    #         raise ValueError("Experience must be an integer between 1 and 10")
-    #     return experience
+    serialize_rules = ("-taskroutines","-users")
 
 def repr(self):
         return f"<Routine {self.id}: {self.name}>"
@@ -90,11 +67,11 @@ class TaskRoutine(db.Model, SerializerMixin):
     routinetemplate_id = db.Column(db.Integer, db.ForeignKey("routinetemplates.id"), nullable=False)
 
     # add relationships
-    # tasktemplate = db.relationship("Tasktemplate", back_populates="taskroutines")
-    # routinetemplate = db.relationship("Routinetemplate", back_populates="taskroutines")
+    tasktemplates = db.relationship("TaskTemplate", back_populates="taskroutines")
+    routinetemplates = db.relationship("RoutineTemplate", back_populates="taskroutines")
 
     # add serialization rules
-    # serialize_rules = ('-owner.visits', '-sitter.visits', '-pet.visits', '-owner.pets', '-sitter.owners')
+    # serialize_rules = ("-users",)
 
     def __repr__(self):
         return f"<Task & Routine: {self.id}>"
